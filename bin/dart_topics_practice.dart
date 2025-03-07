3efae882086832089cea8c6860edf5fc2ff6fe2a
itsrafay03 <itsrafay03@gmail.com>
2025-03-07 14:18:59 +0500

    Dart NullSafety practice is complete.

--- bin/dart_topics_practice.dart
+++ bin/dart_topics_practice.dart
@@ -1,5 +1,5 @@
 import 'package:dart_topics_practice/dart_topics_practice.dart' as dart_topics_practice;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_topics_practice.calculate()}!');
+  print("Programming");
 }
