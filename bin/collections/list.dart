b800e23536ca428818a6793376fcd4a8d50798cd
itsrafay03 <itsrafay03@gmail.com>
2025-05-01 10:41:42 +0500

    Updated code of files

--- bin/collections/list.dart
+++ bin/collections/list.dart
@@ -18,7 +18,7 @@ void main(List<String> args) {
 
   List boolean = <bool>[true, false, false, true];
   List aa = <int>[1, 4, 6, 7];
-  print("Type : ${aa.runtimeType}");
+  print("Type : ${aa.runtimeType}"); // prints List<int>
 
   // Type inference of list as we do not define the D.T of the variable.
   var list6 = [0.12, 0.99, 33, 809];
@@ -27,11 +27,7 @@ void main(List<String> args) {
   ); // It will show List<num> as due to type inference the type of variable depend upon the actual value.
   var list7 = [];
   print(list7.runtimeType); // Now show List<dynamics> as actaual list is empty.
-  list7 = [
-    3,
-    5,
-    6,
-  ]; // Even if list7 is now initilized but still it is List<dynamics>. As first value assign to it is dynamic.
+  list7 = [3, 5, 6]; // Even if list7 is now initilized but still it is List<dynamics>. As first value assign to it is dynamic.
   print(list7.runtimeType);
 
   // Spread Operator.
@@ -43,17 +39,25 @@ void main(List<String> args) {
 
   var list9 = [22, 33, ...list3];
   print(list9);
-  print(
-    list9.runtimeType,
-  ); // List<dynamic> as list9 will be of same D.T as list3 object and list3 is List<dynamic>.
+  print(list9.runtimeType); // List<int> as list9 will be of same D.T as list3 object and list3 is List<int>.
 
-  var list10 = [list9, list4, ...list3];
+  print('----------------------------');
+  var list10 = [list9, list4, list3];
   print(list10);
-  print(
-    list10.runtimeType,
-  ); // List<Object> as list9 is constructed with two differet type of objects of List.
+  print(list10.runtimeType);  // List<List<num>> as it is 2-D list [[],[]]
+  var list11 = [list9, list4, ...list3];
+  print(list11);
+  print(list11.runtimeType);  // List<Object> as list9 is constructed with two differet type of objects like intigers and list.
+  var list12 = [list9, ...list4, ...list3];
+  print(list12);
+  print(list12.runtimeType);
+  var list13 = [...list9, ...list4, ...list3];
+  print(list13);
+  print(list13.runtimeType); // List<num> as all list use spread operator so only their elements will be include in this list13, and they all are int.
 
   // Nullable/Nullsafe Spread Operator.
-  // var list11 = [list2,...nullList!];  // It will be a runtime error as nullList is actually null.
-  var list11 = [list2, ...?nullList];
+  // var list14 = [list2,...nullList!];  // It will be a runtime error as nullList is actually null.
+  var list14 = [list2, ...?nullList];
+
+  
 }
