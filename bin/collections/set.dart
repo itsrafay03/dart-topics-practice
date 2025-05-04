0a7d4aab6e67aa3f3d5420f6491b3b00d8e32502
itsrafay03 <itsrafay03@gmail.com>
2025-05-04 08:56:20 +0500

    Updated code of set file

--- bin/collections/set.dart
+++ bin/collections/set.dart
@@ -1,11 +1,35 @@
-import 'dart:mirrors';
-
-Set<int>? set1;
+Set<String>? set1;
 
 void main(List<String> args) {
-  Set set2 = {1,2,3,2,4};
-  print(set2);
-  print(set2.runtimeType);
+  Set set2 = {1, 2, 3, 2, 4};
+  print(
+    set2.length,
+  ); // As one element is repeated in set so it will not cout here as element.
+  print(set2); // {1, 2, 3, 4} show only unique elements.
+  print(
+    set2.runtimeType,
+  ); // Set<dynamic> as at declaring there is not specific type of set.
 
+  // Cascade operator.
+  set1 = {"Imran", "Khan"};
+  Set<String>? set3 = {'Ali', 'Aleem', 'Akbar', ...?set1};
+  print(set3);
+
+  // Collection if else:
+  Set set4 = {0.1, 0.2, 0.3, if (set2.length == 5) ...set3 else ...set2};
+  print(set4);
+
+  var set5 = set4;
+  print(set5 == set4); // True as they both refer to same object of Set.
+  print(set5); // Same as set4 as they both refer to same object of Set.
+
+  // Collection Terniary(conditional) operator. 
+  var set6 = {(set5.first == 0.1) ? print(1) : print(0)};
+  print(set6); // prints {null} As there is no element in this set.
+  print(
+    set6.runtimeType,
+  ); // Set<void> as no D.T of set is defined and also there is no element in set.
+
+  // Collection switch.
   
 }
