f9646b5eaf289be01472a3a88200c9b4567de4ca
itsrafay03 <itsrafay03@gmail.com>
2025-05-04 09:27:55 +0500

    Updated code of set file

--- bin/collections/set.dart
+++ bin/collections/set.dart
@@ -23,7 +23,7 @@ void main(List<String> args) {
   print(set5 == set4); // True as they both refer to same object of Set.
   print(set5); // Same as set4 as they both refer to same object of Set.
 
-  // Collection Terniary(conditional) operator. 
+  // Collection Terniary(conditional) operator.
   var set6 = {(set5.first == 0.1) ? print(1) : print(0)};
   print(set6); // prints {null} As there is no element in this set.
   print(
@@ -31,5 +31,26 @@ void main(List<String> args) {
   ); // Set<void> as no D.T of set is defined and also there is no element in set.
 
   // Collection switch.
+  num number = 2;
+  var set7 = {
+    'My rollno is',
+    switch (number) {
+      1 => 'One',
+      2 => 'Two',
+      3 => 'Three',
+      _ => 'Other',
+    },
+  };
+  print(set7);
+
+  // Collection for.
+  var set8 = {for (int i = 0; i < 5; i++) i * 2};
+  print(set8);
+
+  // Collection for each.
+  var set9 = {for(int item in set8) item >0 && item<=4 ?[1,2,3,2,3] : {1,2,3,2,3}};
+  print(set9);
+  print(set9.runtimeType); // Set<Iterable<int>> as set9 is collection of different iterables of int
+
   
 }
