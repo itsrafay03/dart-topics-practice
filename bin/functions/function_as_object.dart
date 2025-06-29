a034606202a6be14c76e4209a62f35aa17c4e71e
itsrafay03 <itsrafay03@gmail.com>
2025-06-29 23:17:03 +0500

    Updated code of latest file.

--- bin/functions/function_as_object.dart
+++ bin/functions/function_as_object.dart
@@ -32,6 +32,23 @@ void main(List<String> args){
   // This is how callable object is called. ABC object is made and it's reference will go to 'add' reference variable.
   ABC add = ABC();
   print(add(5, 5));
+
+  Person p = Person('Khan'); // Constrictor call. Person class object is made and it's reference is assigned to 'p' variable.
+  // Simple greet function is called.
+  p.greet();
+  // greet function is called and returned value assign to 'a' variable.
+  String a = p.greet();  
+  // Now we can see Function type variable can not assign to 'p' means it can not refer to data object. Function type variable can only hlod reference of functional object(which is object of callable class).
+  // Function f1 = p;   // Error at p
+  var v = p;
+  print('v: $v');
+
+  // Below is also error because what is happening that p.greet() returned value of String type, so how can we assign String type value to Function type variable. Error due to type mismatch. 
+  // Function f2 = p.greet();
+
+  // Now it's perfect. Because greet is the constant reference variable taht refer to functional object of greet() function. So Function is the parent class of all Callable classes so it can hold the reference of this greet() object.
+  Function f3 = p.greet;
+  
 }
 
 // This is a callable Student class as it has a call function. If we make the Student class object then we can call that object as function with same signature as mentioned in call() function.
@@ -46,5 +63,35 @@ class Student {
 class ABC {
   num call(num no1, num no2){
     return no1 + no2;
+  }
+}
+
+// As function identifier is the constant reference variable of functional object so in Dart we can not make two functions with same name because at a time function identifier(const reference variable) will refer to only one object.
+// That's why there is no function overloading in Dart.
+// We can not make 2nd function with 'sum' identifier even signature is changed.
+
+// int sum(int a){    // Show error
+//   return a + 10;
+// }
+
+// Short form of makeing sum function.
+num sumFunction(num n1, num n2) => n1 + n2;
+// The fat-arrow operator or lambda operator (=>) is used to return value of expression. It is used for single statement. For multiple statement we use {} for body of function.
+
+
+// Very Important point.
+// The constructor is although a special function but in dart like other function the constructor of class is not an object.
+// We does not have an implicit class, object, or identifier as reference of a construct of a class.
+// We can not treat it as regular function because it is specialize function just to create object of class so how could it be an object itself.
+// See it's prove in last lines of void function above.
+class Person {
+  String name;
+
+  // Constructor
+  Person(this.name);
+
+  // Method (regular function)
+  String greet() {
+    return ("Hello, $name");
   }
 }
