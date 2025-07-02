ddee7454b9232f78f483a0b78c387acca3e1b003
itsrafay03 <itsrafay03@gmail.com>
2025-07-02 22:13:46 +0500

    new file of dnamic function execution is added to function folder

---
+++ bin/functions/dynamic_function_execution.dart
@@ -0,0 +1,8 @@
+// Dynamic Function Execution.
+// Dynamic function execution means calling a function at runtime, where the function being called may not be known until 
+// the code is already running — and it might be stored in a variable, passed as an argument, or returned from another function.
+// It’s when you store a function in a variable or pass it around just like data — and then call it later without hardcoding the function name.
+
+void main(List<String> args){
+
+}
---
+++ bin/functions/employee.dart
@@ -0,0 +1,12 @@
+// Employee class for use in Dynamic Function Execution class.
+
+// In Dart (since null safety), every non-nullable field must be initialized:
+// At the point of declaration, or In the constructor before the object is fully created.
+// You're using this.name in the constructor that tells Dart: "This field will definitely get a value when the object is created"
+// therefore, the field is safely initialized, and Dart does not throw an error.
+
+class Employee {
+  String name;
+
+  Employee({required this.name});
+}
