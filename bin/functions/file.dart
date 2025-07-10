ddee19a9f700e0996660eef8940a1cb2bf6ffc3c
itsrafay03 <itsrafay03@gmail.com>
2025-07-11 02:24:17 +0500

    All use cases of initilization list are covered.

--- bin/functions/file.dart
+++ bin/functions/file.dart
@@ -6,6 +6,9 @@ void main(List<String> args) {
   // Above statement when execute it will show exception at assert statement in constructor of Driver class.
   // Because in assert if salary should be greater then 37000 then it will pass this check and assign to salary datamember but here we give salary 35000 so assert catch it and show exception.
 
-  Driver driver2 = Driver(name: 'Waqar', salary: 37500, age: 20);
+  Driver driver2 = Driver(name: 'Waqar', salary: 37500);  // Now this is correct.
   print(driver2);
+
+  // Here we take age because now Driver class is extend with Person class and we have to give the value of parameter of parent class in the constructor of child class. 
+  Driver driver3 = Driver(name: 'Aslam', salary: 120000, age: 27);
 }
