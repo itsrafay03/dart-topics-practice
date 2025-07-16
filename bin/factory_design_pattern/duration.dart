531acb5f73897cf0dc8ae01ded3e2b154c3fd0f4
itsrafay03 <itsrafay03@gmail.com>
2025-07-16 12:11:44 +0500

    Factory constructors of Design patterns are done.

--- bin/factory_design_pattern/duration.dart
+++ bin/factory_design_pattern/duration.dart
@@ -1,6 +1,6 @@
 // This file is made to understand that In Dart, we can use the const keyword with a constructor if we want the object to be a compile-time constant.
 // But for this: All fields of the class must be 'final' and the constructor itself must be marked with the 'const' keyword.
-// Because Dart must guarantee immutability for any object created with const. 'const' means: The object is frozen at compile-time, It will never change in memory, and Dart may reuse the same instance in memory to save space.
+// Because Dart must guarantee immutability for any object created with const. Here 'const' means: The object is frozen at compile-time, It will never change in memory, and Dart may reuse the same instance in memory to save space.
 // Dart enforces final fields and const constructors together so that objects created with const are guaranteed to be fully immutable and safe for compile-time reuse.
 
 class Duration {
@@ -9,8 +9,9 @@ class Duration {
   final int minutes;
   final int seconds;
 
-  // const Constructor.
+  // const Constructor. We use const with a constructor to enable compile-time constant object creation. Immutablity prevents modification after creation.
   const Duration({this.hours = 0, this.minutes = 0, this.seconds = 0});
+  // A generative constructor creates a new instance of a class. A const constructor is a special kind of generative constructor that can create an object at compile time, returns a new object only if it doesn't already exist with the same values, otherwise Dart reuses the previously created constant instance.
 }
 
 
@@ -24,4 +25,4 @@ class Person {
 
 // Since name is not final, it could be changed later.
 // That violates the promise of const being immutable.
-// Therefore, Dart shows a compile-time error.
+// Therefore, Dart shows a compile-time error.   
--- bin/factory_design_pattern/main.dart
+++ bin/factory_design_pattern/main.dart
@@ -1,20 +1,21 @@
 import 'border.dart';
 import 'shape.dart';
+import 'duration.dart' as d;
 // This file has a main function to construct and execute the objects of Border and Shape class.
 
-void main(List<String> args){
+void main(List<String> args) {
   Border border1 = Border(BorderType.singleBorder);
   Border border2 = Border(BorderType.singleBorder);
   Border border3 = Border(BorderType.singleBorder);
   Border border4 = Border(BorderType.singleBorder);
   // If we did not implement Factory Design Pattern in Border class then above 4 different objects of Border class will form and each object have same data in it but just they have 4 memory blocks occupied.
-  // But as we implement Factory Design Pattern in Border class so when every we construct Border object with singleBorder then it refer the already made object in memory. So in above case all 4 references are refering to single object in memory. 
-  
+  // But as we implement Factory Design Pattern in Border class so when every we construct Border object with singleBorder then it refer the already made object in memory. So in above case all 4 references are refering to single object in memory.
+
   print('Shapes');
   Shape rect = Shape(Shapes.rectangle);
   Shape circle = Shape(Shapes.circle);
   Shape triangle = Shape(Shapes.triangle);
-  Rectangle rectangle = Shape(Shapes.rectangle) as Rectangle; 
+  Rectangle rectangle = Shape(Shapes.rectangle) as Rectangle;
   // We need to cast because child class can not have a reference of parent class.
 
 // How can we call the constructor of Shape class as it is an abstract class?
@@ -24,4 +25,13 @@ void main(List<String> args){
 // The result is an object of type Triangle, but stored in a variable of type Shape. That's how you're able to return child objects from an abstract parent class using the factory constructor.
 // In current logic very time a new object of a child class is constructed from factory constructor of parent class.
 
+// Here above we import our custome Duration class is import as 'd'.
+  d.Duration duration = const d.Duration();
+  print(duration.hours);
+  print(duration.minutes);
+  print(duration.seconds);
+  // duration.minutes = 23;  // Show error at minutes because it's fields are constant which can not be change. 
+
+  // Orignal class of Duration.
+  Duration durat = const Duration();
 }
