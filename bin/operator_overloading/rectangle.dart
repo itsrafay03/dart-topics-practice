6fe3c596e6d7c7542d8c6fe3460d3c7a90660b1d
itsrafay03 <itsrafay03@gmail.com>
2025-07-17 01:33:52 +0500

    Practice files of operator overloading is added.

--- bin/operator_overloading/rectangle.dart
+++ bin/operator_overloading/rectangle.dart
@@ -21,7 +21,7 @@ class Rectangle {
   num get width => _width;
   set width(num input) {
     if (input > 0.0) {
-      width = input;
+      _width = input;
     } else {
       throw Exception('$input must be greater then 0');
     }
@@ -32,29 +32,36 @@ class Rectangle {
   Rectangle({num h = 1, num w = 1})
       : _height = h,
         _width = w;
+  // In above constructor if user pass -ve value it will also assign to fields because initilization is occuring in initilization list which executes before the body of constructor.
+  // As we can only use setters and getters in function body. So if we want to use setters to set values of fields according to abstraction then we have to use property in bode of constructor like below:
+  // Rectangle({num h = 1, num w = 1}) {
+  //   height = h;   // uses setter
+  //   width = w;   // uses setter
+  // }
 
   // Named Constructor with constructor redirecting.
   Rectangle.fromJson(String json) : this.fromMap(jsonDecode(json));
-  Rectangle.fromMap(Map<String, dynamic> map) : this(h: map['height'], w: map['width']);      
+  Rectangle.fromMap(Map<String, dynamic> map)
+      : this(h: map['height'], w: map['width']);
 
   // So called Operator Overloading.
-  bool operator ==(covariant Rectangle object){
+  bool operator ==(covariant Rectangle object) {
     return height == object._height && _width == object._width;
-  } 
+  }
 
-  bool operator >=(Rectangle object){
+  bool operator >=(Rectangle object) {
     return _height > object._height && _width == object._width;
   }
 
-  bool operator <=(Rectangle object){
+  bool operator <=(Rectangle object) {
     return _width < object._width && _height == object._height;
-  } 
+  }
 
-  // We use Property(getters) in below.  
-  Rectangle operator +(Rectangle object){
+  // We use Property(getters) in below.
+  Rectangle operator +(Rectangle object) {
     return Rectangle(h: height + object.height, w: width + object.width);
   }
-  
+
   // toString method.
   @override
   String toString() {
