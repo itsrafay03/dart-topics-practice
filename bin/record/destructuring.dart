386d0777d767b9687f00046a48a0261f32ee458e
itsrafay03 <itsrafay03@gmail.com>
2025-07-22 22:55:42 +0500

    Destructuring of class constructor is also added to code

--- bin/record/destructuring.dart
+++ bin/record/destructuring.dart
@@ -2,7 +2,7 @@
 
 void main(List<String> args) {
   // Part 5: Destructuring.
-  // Destructuring in record types refers to the process of extracting individual values from a record and assigning them to separate variables. 
+  // Destructuring in record types refers to the process of extracting individual values from a record and assigning them to separate variables.
   // But this will be done through pattern matching, if pattern match then they will destructure otherwise not.
   // Destructuring of Positional fields records.
   // We use 'var' for type inference of variables name and age. The destructuring pattern on the left matches the positional record structure on the right, which is a record of two positional values: ('Java', 54). As record matched so the first value is assigned to name, and the second to age, with their types inferred based on the assigned values.
@@ -17,32 +17,37 @@ void main(List<String> args) {
   var (digit, word) = foo();
   //or
   // var (int digit, String word) = foo();   // It's same like above.
-  print(word);   // Ali
+  print(word); // Ali
 
-  var info = ('Python', false, {2,3,4});
+  var info = ('Python', false, {2, 3, 4});
   // info is a record of positional fields, now it is destructure.
-  var (langName, isEasy, setOfValues) = info;  
-  print(info.$3);   // instead of this we can write below one because info record is destructured and it's 3rd field assign to the variable 'setOfValues'.
+  var (langName, isEasy, setOfValues) = info;
+  print(info
+      .$3); // instead of this we can write below one because info record is destructured and it's 3rd field assign to the variable 'setOfValues'.
   print(setOfValues);
 
-  // Destructuring of Named fields records. 
+  // Destructuring of Named fields records.
   // We use colon':' for the destructuring of named fields of record. For destructuring of named filed record we use key value pairs. The key here is name of field and value is the variable name that we want for the field.
-  // In named record destructuring, we use the pattern fieldName: variableName to extract values. The 3 named field record (name: 'Swift', company: 'Apple', duration: 12) is destructured into 3 variables n, c, and d. 
+  // In named record destructuring, we use the pattern fieldName: variableName to extract values. The 3 named field record (name: 'Swift', company: 'Apple', duration: 12) is destructured into 3 variables n, c, and d.
   // Dart infers their types based on the assigned values, and var enables that inference.
-  var (name: n, company: c, duration: d) = (name: 'Swift', company: 'Apple', duration: 12);
+  var (name: n, company: c, duration: d) =
+      (name: 'Swift', company: 'Apple', duration: 12);
   print('Duration of $n is $d');
 
   // If we want the same variable names as the field names of record then we can simplify using (:fieldName, :fieldName).
-  var (:languageName, :company, :duration) = (languageName: 'Swift', company: 'Apple', duration: 12);
+  var (:languageName, :company, :duration) =
+      (languageName: 'Swift', company: 'Apple', duration: 12);
   print(languageName);
   print(company);
-  
+
   // Destructure record of named fields which is returning from a fuction.
   // As they are named fields so we can change their order according to fields name.
-  var (:studentRollNo, : studentName) = studentInfo({'name':'Devon Larat', 'address':'Bwp city', 'rollNo': 29});
+  var (:studentRollNo, :studentName) =
+      studentInfo({'name': 'Devon Larat', 'address': 'Bwp city', 'rollNo': 29});
   print("$studentName rollNo is $studentRollNo");
   // Or we can do that:
-  var data = studentInfo({'name':'Iron Man', 'address':'West city', 'rollNo': 71});
+  var data =
+      studentInfo({'name': 'Iron Man', 'address': 'West city', 'rollNo': 71});
   var (studentName: identifier, studentRollNo: roll) = data;
   print("$identifier rollNo is $roll");
 
@@ -50,48 +55,70 @@ void main(List<String> args) {
   // Part 6: Immutability of fields of record.
   // This is positional record destructuring with var → meaning variables are mutable.
   var (String place, int index) = ("UAE", 50);
-  print(place);  // UAE
+  print(place); // UAE
   place = 'KSA';
-  print(place);  // KSA
+  print(place); // KSA
 
   // This is named record destructuring with var → meaning variables are mutable.
   var (:region, :point) = (region: 'Pakistan', point: 100);
-  print(region);  // Pakistan
+  print(region); // Pakistan
   region = 'Brazil';
-  print(region);  // Brazil
+  print(region); // Brazil
 
   // By default, variables created through record destructuring using var are mutable, meaning their values can be changed after assignment.
   // If you want the destructured variables to be immutable (i.e., their values can't be changed once assigned), use final instead of var.
   // This makes each variable a runtime constant, allowing the value to be assigned only once during execution.
   // Using final is especially useful for maintaining immutability, predictable and safe state in your code.
-  
+
   // This is positional record destructuring with final → meaning variables are immutable.
   final (String places, int indexs) = ("UAE", 50);
-  print(places);  // UAE
+  print(places); // UAE
   // places = 'KSA';  // Here we get error that final variable 'places' can only be set once.
 
   // This is named record destructuring with final → meaning variables are immutable.
   final (:regions, :points) = (regions: 'Pakistan', points: 100);
-  print(regions);  // Pakistan
+  print(regions); // Pakistan
   // regions = 'Brazil';  // The final variable 'regions' can only be set once.
 
-//  You cannot use const directly for destructured variables because const requires values to be known at compile time, and destructuring often happens at runtime.  
-// Dart only allows const with variables declared at compile-time and where the entire value must be known at compile time.
-// But Dart does not currently support this syntax:
-// const (a, b) = (1, 2); // ❌ Error: const destructuring is not allowed like this.
-// ✅ Instead, you can do:
-// const record = (1, 2);
-// final (a, b) = record;  // ✅ Now this is fine.
+  // You cannot use const directly for destructured variables because const requires values to be known at compile time, and destructuring often happens at runtime.
+  // Dart only allows const with variables declared at compile-time and where the entire value must be known at compile time.
+  // But Dart does not currently support this syntax:
+  // const (a, b) = (1, 2); // ❌ Error: const destructuring is not allowed like this.
+  // ✅ Instead, you can do:
+  const record = (1, 2);
+  final (first, second) = record; // ✅ Now this is fine.
+  print(first);
+  // first = 5;   // The final variable 'first' can only be set once.
 
 
-} 
+  // Part 7: Destructuring of Class constructor.
+  // Just like records, any composite type (i.e., a type made up of multiple fields or values) can be destructured using pattern matching in Dart. This includes classes — but only if they have a matching constructor and accessible fields.
+  // Dart supports destructuring via class patterns, which allow you to extract individual fields from class instances.
+  var size = Size(height: 100, width: 200);
+  print(size);
+  print(size.runtimeType);  // size, because it refer to the object of Size class.
+  // For destructuring of constructor we have to 
+  var Size(width: w, height: h) = size;
+  print(w);
+  // or we can do.
+  var Size(:height, :width) = Size(height: 190, width: 77);
+  print("Height: $height, Width: $width");
+}
 
 // If we want to return multiple type of values from function so we can use record as RDT. In actual only single type value is returning from function which is record. But record is composit type.
-(num, String) foo(){
+(num, String) foo() {
   return (20, 'Ali');
 }
 
 // Another function which return multiple types. Although function always retun a single type value like here record. But record itself is combination of different types.
-({String studentName, int studentRollNo}) studentInfo(Map<String, dynamic> map){
+({String studentName, int studentRollNo}) studentInfo(
+    Map<String, dynamic> map) {
   return (studentName: map['name'], studentRollNo: map['rollNo']);
+}
+
+// A simple class with 2 named fields in constructor.
+class Size {
+  double height;
+  double width;
+  Size({required this.height, required this.width});
 }
